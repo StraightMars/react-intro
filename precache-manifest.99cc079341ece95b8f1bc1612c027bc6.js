32ef0290abb5dcf875c09327021fc29c2695944d
StraightMars <mnurev@mail.ru>
2020-01-24 01:14:53 +0500

    Updates

---
+++ precache-manifest.99cc079341ece95b8f1bc1612c027bc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd49a422a28cb2b466d01e4a03b237c",
+    "url": "/react-intro/index.html"
+  },
+  {
+    "revision": "2e972dc78f8a8c8336fa",
+    "url": "/react-intro/static/css/main.e7e9118f.chunk.css"
+  },
+  {
+    "revision": "1babc1f714a428e07730",
+    "url": "/react-intro/static/js/2.2b7def97.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-intro/static/js/2.2b7def97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e972dc78f8a8c8336fa",
+    "url": "/react-intro/static/js/main.695e773a.chunk.js"
+  },
+  {
+    "revision": "d72c7bab3b4adb775fbe",
+    "url": "/react-intro/static/js/runtime-main.5136680a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-intro/static/media/logo.5d5d9eef.svg"
+  }
+]);
